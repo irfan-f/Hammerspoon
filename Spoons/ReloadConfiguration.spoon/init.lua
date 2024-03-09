49e2f9050d498e84c075c84e0b805766825f66ae
irfan-f <irfan.filipovic23@gmail.com>
2024-03-09 08:58:39 -0800

    menubar item now created and functional

--- Spoons/ReloadConfiguration.spoon/init.lua
+++ Spoons/ReloadConfiguration.spoon/init.lua
@@ -2,16 +2,33 @@
 ---
 --- Provides an easy way to start and stop watching the Hammerspoon directory to live reload the configuration.
 ---
+
 local obj = {}
 obj.__index = obj
-
-local _watcher = nil
 
 -- Metadata
 obj.name = "ReloadConfiguration"
 obj.version = "0.1.0"
 obj.author = "Irfan F."
 obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+-- Private variables
+local _watcher = nil
+
+-- Menubar
+--- @type hs.menubar | nil
+local menubar
+-- Menubar message strings
+local menubarPrefix = "HS reload: "
+local menubarRefreshIcon = hs.image.systemImageNames.RefreshTemplate
+local menubarStopIcon = hs.image.systemImageNames.StopProgressTemplate
+
+-- Menubar click callback
+local function menubarClicked()
+  obj:toggle()
+end
+
+
 
 --- ReloadConfiguration:start()
 --- Method
@@ -24,10 +41,13 @@ obj.license = "MIT - https://opensource.org/licenses/MIT"
 ---  * None
 ---
 function obj:start()
+  -- If the watcher is already running, stop it
   if _watcher ~= nil then
     self:stop()
   end
+  -- Create a new watcher and start it
   _watcher = hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', function(files)
+    -- If any .lua file changes, reload the configuration
     local doReload = false
     for _, file in pairs(files) do
       if file:sub(-4) == ".lua" then
@@ -38,7 +58,12 @@ function obj:start()
       hs.reload()
     end
   end):start()
-  hs.alert.show("Watching for live configuration reload")
+
+  -- Update the menubaritem to indicate that the watcher is running
+  if menubar ~= nil then
+    menubar:setTooltip("Click to stop watching the Hammerspoon directory")
+    menubar:setIcon(hs.image.imageFromName(menubarRefreshIcon))
+  end
 end
 
 --- ReloadConfiguration:stop()
@@ -52,10 +77,15 @@ end
 ---  * None
 ---
 function obj:stop()
+  -- If the watcher is running, stop it
   if _watcher ~= nil then
     _watcher:stop()
-    hs.alert.show("Stopped watching for live configuration reload")
     _watcher = nil
+    -- Update the menubaritem to indicate that the watcher has stopped
+    if menubar ~= nil then
+      menubar:setTooltip("Click to start watching the Hammerspoon directory")
+      menubar:setIcon(hs.image.imageFromName(menubarStopIcon))
+    end
   end
 end
 
@@ -70,10 +100,44 @@ end
 ---  * None
 ---
 function obj:toggle()
+  -- If the watcher is running, stop it. Otherwise, start it
   if _watcher == nil then
     self:start()
   else
     self:stop()
+  end
+end
+
+--- ReloadConfiguration:init()
+--- Method
+--- Initialize the Spoon
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
+---
+--- Notes:
+---  * This method is called when the Spoon is loaded
+---  * It sets up the menubar item
+function obj:init()
+  -- Set the menubar title to indicate that the watcher is stopped
+  menubar = hs.menubar.new()
+
+  -- Set the click callback
+  if menubar then
+    menubar:setClickCallback(menubarClicked)
+  end
+end
+
+function obj:toggleMenuItem()
+  if menubar and menubar:isInMenuBar() then
+    menubar:removeFromMenuBar()
+  end
+
+  if menubar and ~menubar:isInMenuBar() then
+    menubar:returnToMenuBar()
   end
 end
 
